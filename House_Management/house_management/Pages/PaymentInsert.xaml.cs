a1e8abb9c7506bb6ae439de602c03bd915d8f412
stilzi <aslan12.g@mail.ru>
2021-04-09 08:38:01 +0300

    house management

---
+++ House_Management/house_management/Pages/PaymentInsert.xaml.cs
@@ -0,0 +1,63 @@
+﻿using house_management.Classes;
+using house_management.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace house_management.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PaymentInsert.xaml
+    /// </summary>
+    public partial class PaymentInsert : Page
+    {
+        public PaymentInsert()
+        {
+            InitializeComponent();
+            TypeOfPaymentcmb.ItemsSource = dbContext.db.TypeOfPayment.Select(item => item.Title).ToList();
+        }
+
+        /// <summary>
+        /// Перейти на предыдущую страницу
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        /// <summary>
+        /// Добавление данных
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Insert_Click(object sender, RoutedEventArgs e)
+        {
+            var insertTypeOfPayment = dbContext.db.TypeOfPayment.FirstOrDefault(item => item.Title == TypeOfPaymentcmb.Text);
+            Payment insertPayment = new Payment()
+            {
+                TypeOfPaymentId = insertTypeOfPayment.IdTypeOfPayment,
+                SquarePrice = Convert.ToInt32(txbSquarePrice.Text),
+                LodgersPrice = Convert.ToInt32(txbLodgersPrice.Text),
+                PaymentAmount = Convert.ToInt32(txbPaymentAmount.Text),
+                SupposedDatePayment = Convert.ToDateTime(dtSupposedDatePayment.SelectedDate),
+                DateOfPayment = Convert.ToDateTime(dtDateOfPayment.SelectedDate)
+            };
+            dbContext.db.Payment.Add(insertPayment);
+            dbContext.db.SaveChanges();
+            MessageBox.Show("Добавдено", "Добавление", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ House_Management/house_management/Pages/PaymentPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using house_management.Classes;
+using house_management.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace house_management.Pages
+{
+    public partial class PaymentPage : Page
+    {
+        public PaymentPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Вывод таблицы Payment в DataGrid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            PaymentView.ItemsSource = dbContext.db.Payment.ToList();
+        }
+
+        /// <summary>
+        /// Редактирование данных
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            dbContext.db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Возвращение в главное меню
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        /// <summary>
+        /// Удаления данных
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            Payment removePayment = (Payment)PaymentView.SelectedItem;
+            dbContext.db.Payment.Remove(removePayment);
+            dbContext.db.SaveChanges();
+            PaymentView.ItemsSource = dbContext.db.Payment.ToList();
+        }
+
+        /// <summary>
+        /// Переход на страницу добавления данных
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void InsertHouseOpen_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new PaymentInsert());
+        }
+    }
+}
